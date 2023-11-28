7ab9973ea55780fda303b452c6b517c15f18d2fa
Taemin <kwaktaemin98@gmail.com>
2023-11-28 17:00:04 +0900

    initalize

--- kaykaytalk_rest/src/app.module.ts
+++ kaykaytalk_rest/src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ChatGateway } from './chat/chat.gateway';
+import { ConfigModule } from '@nestjs/config';
+import { MongoDbModule } from './mongo-db/mongo-db.module';
+import { MysqlModule } from './mysql/mysql.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    MongoDbModule,
+    MysqlModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ChatGateway],
 })
 export class AppModule {}
