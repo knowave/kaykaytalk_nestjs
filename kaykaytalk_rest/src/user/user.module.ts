faa79babba5a5dbd7244d49231ebdc0030557f84
Taemin <kwaktaemin98@gmail.com>
2023-12-14 18:14:36 +0900

    제거: DataSource 의존성 주입에서 제거

--- kaykaytalk_rest/src/user/user.module.ts
+++ kaykaytalk_rest/src/user/user.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { UserRepository } from './user.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
-import { DataSource } from 'typeorm';
+import { UserService } from './user.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
-  providers: [UserService, UserRepository, DataSource],
+  providers: [UserService, UserRepository],
 })
 export class UserModule {}
