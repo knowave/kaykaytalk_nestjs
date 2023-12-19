0866aa33e0e5c3cf5dd27734dad9f242c8985556
Taemin <tm.kwak@chopsticks.market>
2023-12-19 19:09:21 +0900

    추가: token 검증 메서드 추가

--- kaykaytalk_rest/src/auth/auth.service.ts
+++ kaykaytalk_rest/src/auth/auth.service.ts
@@ -1,26 +1,19 @@
 import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
-  }
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {}
 
-  findAll() {
-    return `This action returns all auth`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+  async tokenValidate(token: string) {
+    return await this.jwtService.verify(token, {
+      secret: this.configService.get('JWT_SECRET'),
+    });
   }
 }
