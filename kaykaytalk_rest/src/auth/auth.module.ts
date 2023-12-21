35b024245c779f6e482ae48889ec13c1373f9d2b
Taemin <tm.kwak@chopsticks.market>
2023-12-21 10:34:42 +0900

    추가

--- kaykaytalk_rest/src/auth/auth.module.ts
+++ kaykaytalk_rest/src/auth/auth.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserModule } from 'src/user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/user/entities/user.entity';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule, JwtService } from '@nestjs/jwt';
 
 @Module({
-  imports: [UserModule],
-  controllers: [],
-  providers: [AuthService],
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('JWT-SECRET'),
+        signOptions: { expiresIn: '60m' },
+      }),
+    }),
+    TypeOrmModule.forFeature([User]),
+    PassportModule,
+    ConfigModule,
+    UserModule,
+  ],
+  providers: [AuthService, JwtService, ConfigService],
 })
 export class AuthModule {}
