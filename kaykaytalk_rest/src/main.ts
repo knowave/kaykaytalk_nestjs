31636aa4e9de9d6f2b7b28769a9dcd0cf1ebcfba
knowave <kwaktaemin98@gmail.com>
2023-12-27 10:18:09 +0900

    추가: validation pipe추가

--- kaykaytalk_rest/src/main.ts
+++ kaykaytalk_rest/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  useValidationPipe(app);
+
   await app.listen(3000);
+
+  function useValidationPipe(app: INestApplication): void {
+    const pipe = new ValidationPipe();
+    app.useGlobalPipes(pipe);
+  }
 }
 bootstrap();
