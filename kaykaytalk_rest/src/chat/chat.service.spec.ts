389bcb92588716c03b0b9d87293ec77f17ed8a39
knowave <kwaktaemin98@gmail.com>
2023-12-27 16:22:43 +0900

    수정: test code 실행 시 발생하는 Error를 없애기 위하여 주석처리 제거하여 복원

--- kaykaytalk_rest/src/chat/chat.service.spec.ts
+++ kaykaytalk_rest/src/chat/chat.service.spec.ts
@@ -1,18 +1,35 @@
-// import { Test, TestingModule } from '@nestjs/testing';
-// import { ChatService } from './chat.service';
+import { Test, TestingModule } from '@nestjs/testing';
+import { ChatService } from './chat.service';
+import { Chat } from './entities/chat.entity';
+import { Model } from 'mongoose';
+import { getModelToken } from '@nestjs/mongoose';
 
-// describe('ChatService', () => {
-//   let service: ChatService;
+const mockChatModel = () => ({
+  save: jest.fn(),
+  find: jest.fn(),
+  findOne: jest.fn(),
+});
 
-//   beforeEach(async () => {
-//     const module: TestingModule = await Test.createTestingModule({
-//       providers: [ChatService],
-//     }).compile();
+describe('ChatService', () => {
+  let service: ChatService;
+  let chatModel: Model<Chat>;
 
-//     service = module.get<ChatService>(ChatService);
-//   });
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        ChatService,
+        {
+          provide: getModelToken('Chat'),
+          useValue: mockChatModel(),
+        },
+      ],
+    }).compile();
 
-//   it('should be defined', () => {
-//     expect(service).toBeDefined();
-//   });
-// });
+    service = module.get<ChatService>(ChatService);
+    chatModel = module.get<Model<Chat>>(getModelToken('Chat'));
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
