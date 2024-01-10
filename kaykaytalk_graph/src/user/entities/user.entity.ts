868880c89258badd2d528d3ed056fb1aad34d062
knowave <kwaktaemin98@gmail.com>
2024-01-10 16:12:14 +0900

    추가: user entity

--- kaykaytalk_graph/src/user/entities/user.entity.ts
+++ kaykaytalk_graph/src/user/entities/user.entity.ts
@@ -1,7 +1,24 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Entity, Property } from '@mikro-orm/core';
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import { BaseEntity } from 'src/common/base.entity';
 
+@InputType('userInputType')
 @ObjectType()
-export class User {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+@Entity()
+export class User extends BaseEntity {
+  @Field(() => String)
+  @Property({ columnType: 'varchar', comment: '사용자 이메일 주소' })
+  email: string;
+
+  @Field(() => String)
+  @Property({ columnType: 'varchar', comment: '사용자 이름' })
+  username: string;
+
+  @Field(() => String)
+  @Property({ columnType: 'varchar', comment: '사용자 비밀번호' })
+  password: string;
+
+  @Field(() => String)
+  @Property({ columnType: 'varchar', comment: 'jwt refresh token' })
+  refreshToken: string;
 }
