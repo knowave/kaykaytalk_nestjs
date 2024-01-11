8c63763d42d43a8acfe1d8f0ad561cb957691cf7
knowave <kwaktaemin98@gmail.com>
2024-01-11 14:27:46 +0900

    추가

---
+++ kaykaytalk_graph/src/config/mysql/mysql.module.ts
@@ -0,0 +1,36 @@
+import { MySqlDriver } from '@mikro-orm/mysql';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    MikroOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        driver: MySqlDriver,
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        user: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        dbName: configService.get('DB_NAME'),
+        autoLoadEntities: true,
+        schemaGenerator: {
+          disableForeignKeys: true,
+          createForeignKeyConstraints: true,
+          ignoreSchema: [],
+        },
+        entities: ['../../**/entities/**/*.entity.{ts,js}'],
+        entitiesTs: ['../../**/entities/**/*.entity.{ts,js}'],
+        baseDir: __dirname,
+        migrations: {
+          path: './migrations/*.ts',
+          tableName: 'migrations',
+          transactional: true,
+        },
+      }),
+    }),
+  ],
+})
+export class MysqlModule {}
