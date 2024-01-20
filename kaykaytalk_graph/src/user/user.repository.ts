1da4202b071873f55e5673107a04357c7afa3427
knowave <kwaktaemin98@gmail.com>
2024-01-20 16:04:31 +0900

    추가 getUserByEmail

--- kaykaytalk_graph/src/user/user.repository.ts
+++ kaykaytalk_graph/src/user/user.repository.ts
@@ -1,13 +1,28 @@
-import { EntityRepository } from '@mikro-orm/core';
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { EntityManager, EntityRepository } from '@mikro-orm/core';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserRepository extends EntityRepository<User> {
+  constructor(private readonly entityManager: EntityManager) {
+    super(entityManager, User);
+  }
   async getUserById(userId: number): Promise<User> {
     const user = await this.findOne({ id: userId });
 
     if (!user) throw new NotFoundException('존재하는 user가 없습니다.');
+
+    return user;
+  }
+
+  async getUserByEmail(email: string): Promise<User> {
+    const user = await this.findOne({ email });
+
+    if (user) throw new BadRequestException('이미 존재하는 이메일입니다.');
 
     return user;
   }
