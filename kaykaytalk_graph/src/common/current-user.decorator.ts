3be8da2467679365aa21ff4d2645f393240f749f
knowave <kwaktaemin98@gmail.com>
2024-01-20 16:58:04 +0900

    생성: user decorator

---
+++ kaykaytalk_graph/src/common/current-user.decorator.ts
@@ -0,0 +1,11 @@
+import { ExecutionContext, createParamDecorator } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
+
+export const CurrentUser = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const gqlContext = GqlExecutionContext.create(ctx).getContext();
+    const { password, hashedPassword, ...results } = gqlContext['user'];
+
+    return results;
+  },
+);
