309557e1cb728c5e7701a7d6c67607d05d5cc77c
knowave <kwaktaemin98@gmail.com>
2024-01-20 16:30:25 +0900

    수정: queryBuilder로 수정

--- kaykaytalk_graph/src/user/user.repository.ts
+++ kaykaytalk_graph/src/user/user.repository.ts
@@ -1,4 +1,4 @@
-import { EntityManager, EntityRepository } from '@mikro-orm/core';
+import { EntityManager, EntityRepository } from '@mikro-orm/mysql';
 import {
   BadRequestException,
   Injectable,
@@ -12,7 +12,7 @@ export class UserRepository extends EntityRepository<User> {
     super(entityManager, User);
   }
   async getUserById(userId: number): Promise<User> {
-    const user = await this.findOne({ id: userId });
+    const user = await this.qb('user').where({ id: userId }).getSingleResult();
 
     if (!user) throw new NotFoundException('존재하는 user가 없습니다.');
 
@@ -20,7 +20,7 @@ export class UserRepository extends EntityRepository<User> {
   }
 
   async getUserByEmail(email: string): Promise<User> {
-    const user = await this.findOne({ email });
+    const user = await this.qb('user').where({ email }).getSingleResult();
 
     if (user) throw new BadRequestException('이미 존재하는 이메일입니다.');
 
