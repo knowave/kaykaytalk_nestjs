c44753d31e22efb6769eab1634179ef5a58ed8f9
knowave <kwaktaemin98@gmail.com>
2024-01-20 15:58:46 +0900

    추가: createUser 추가

--- kaykaytalk_graph/src/user/user.resolver.ts
+++ kaykaytalk_graph/src/user/user.resolver.ts
@@ -1,7 +1,9 @@
-import { Resolver, Query, Args } from '@nestjs/graphql';
+import { Resolver, Query, Args, Mutation } from '@nestjs/graphql';
 import { UserService } from './user.service';
 import { GetUserByIdOutput } from './dto/output/get-user-by-id.output';
 import { GetUserByIdInput } from './dto/input/get-user-by-id.input';
+import { CreateUserOutput } from './dto/output/create-user.output';
+import { CreateUserInput } from './dto/input/create-user.input';
 
 @Resolver()
 export class UserResolver {
@@ -12,5 +14,12 @@ export class UserResolver {
     @Args('input') getUserByIdInput: GetUserByIdInput,
   ): Promise<GetUserByIdOutput> {
     return this.userService.getUserById(getUserByIdInput);
+  }
+
+  @Mutation(() => CreateUserOutput)
+  async createUser(
+    @Args('input') createUserInput: CreateUserInput,
+  ): Promise<CreateUserOutput> {
+    return this.userService.createUser(createUserInput);
   }
 }
--- kaykaytalk_graph/src/user/user.service.ts
+++ kaykaytalk_graph/src/user/user.service.ts
@@ -1,16 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { GetUserByIdInput } from './dto/input/get-user-by-id.input';
 import { GetUserByIdOutput } from './dto/output/get-user-by-id.output';
-import { InjectRepository } from '@mikro-orm/nestjs';
 import { UserRepository } from './user.repository';
+import { EntityManager } from '@mikro-orm/mysql';
+import { User } from './entities/user.entity';
+import { CreateUserInput } from './dto/input/create-user.input';
+import { CreateUserOutput } from './dto/output/create-user.output';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
-  constructor(private readonly userRepository: UserRepository) {}
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly entityManager: EntityManager,
+  ) {}
 
   async getUserById({ userId }: GetUserByIdInput): Promise<GetUserByIdOutput> {
     const user = await this.userRepository.getUserById(userId);
 
     return { ok: true, user };
+  }
+
+  async createUser({
+    email,
+    username,
+    password,
+  }: CreateUserInput): Promise<CreateUserOutput> {
+    const em = this.entityManager.fork();
+    await em.begin();
+    try {
+      const createUser = em.create(User, {
+        email,
+        username,
+        password: await bcrypt.hash(password, 10),
+      });
+
+      em.persist(createUser);
+      await em.commit();
+      return { ok: true };
+    } catch (err) {
+      await em.rollback();
+      throw new InternalServerErrorException(`Server Error: ${err.stack}`);
+    }
   }
 }
