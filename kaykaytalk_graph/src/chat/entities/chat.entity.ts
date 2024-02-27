46f21d7d69b32664749270d8c434ab863c98677b
knowave <kwaktaemin98@gmail.com>
2024-02-27 17:22:25 +0900

    Generate Chat Entity

---
+++ kaykaytalk_graph/src/chat/entities/chat.entity.ts
@@ -0,0 +1,24 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type ChatDocument = HydratedDocument<Chat>;
+
+@Schema()
+export class Chat {
+  @Prop()
+  userId: number;
+
+  @Prop()
+  roomId: number;
+
+  @Prop()
+  messageId: number;
+
+  @Prop()
+  username: string;
+
+  @Prop()
+  content: string;
+}
+
+export const ChatSchema = SchemaFactory.createForClass(Chat);
