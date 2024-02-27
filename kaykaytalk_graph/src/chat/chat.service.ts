6612cc0c4931a84de309895b65a83fa18ee398a6
knowave <kwaktaemin98@gmail.com>
2024-02-27 17:25:39 +0900

    chat business logic

--- kaykaytalk_graph/src/chat/chat.service.ts
+++ kaykaytalk_graph/src/chat/chat.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateChatDto } from './dto/create-chat.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Chat } from './entities/chat.entity';
+import { Model } from 'mongoose';
+import { UpdateChatDto } from './dto/update-chat.dto';
 
 @Injectable()
-export class ChatService {}
+export class ChatService {
+  constructor(
+    @InjectModel(Chat.name) private readonly ChatModel: Model<Chat>,
+  ) {}
+
+  async createChat(createChatDto: CreateChatDto) {
+    const saveChat = new this.ChatModel(createChatDto);
+    return saveChat.save();
+  }
+
+  async getAllChatByUserId(userId: number, roomId: number) {
+    const chats = await this.ChatModel.find({ userId, roomId });
+    return chats;
+  }
+
+  async updateChatMessage(userId: number, updateChatDto: UpdateChatDto) {
+    const chatMessage = await this.ChatModel.findOne({
+      userId,
+      messageId: updateChatDto.messageId,
+    });
+
+    if (!chatMessage) throw new NotFoundException();
+
+    chatMessage.content = updateChatDto.content;
+
+    return await chatMessage.save();
+  }
+}
---
+++ kaykaytalk_graph/src/chat/dto/create-chat.dto.ts
@@ -0,0 +1,6 @@
+export class CreateChatDto {
+  userId: number;
+  roomId: number;
+  username: string;
+  content: string;
+}
